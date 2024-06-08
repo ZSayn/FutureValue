d27afd646965690573e4c3058bf6c85473664ade
raxmonali <raxmonaliabduvaliyev077@gmail.com>
2024-06-08 13:13:56 +0500

    Exeption handling

--- FutureValue/Form1.Designer.cs
+++ FutureValue/Form1.Designer.cs
@@ -43,74 +43,80 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(31, 43);
+            label1.Location = new Point(27, 32);
             label1.Name = "label1";
-            label1.Size = new Size(142, 20);
+            label1.Size = new Size(117, 15);
             label1.TabIndex = 0;
             label1.Text = "Monthly Investment:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(31, 86);
+            label2.Location = new Point(27, 64);
             label2.Name = "label2";
-            label2.Size = new Size(138, 20);
+            label2.Size = new Size(109, 15);
             label2.TabIndex = 1;
             label2.Text = "Yearly Interest Rate:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(34, 137);
+            label3.Location = new Point(30, 103);
             label3.Name = "label3";
-            label3.Size = new Size(122, 20);
+            label3.Size = new Size(98, 15);
             label3.TabIndex = 2;
             label3.Text = "Number of Years:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(34, 179);
+            label4.Location = new Point(30, 134);
             label4.Name = "label4";
-            label4.Size = new Size(93, 20);
+            label4.Size = new Size(75, 15);
             label4.TabIndex = 3;
             label4.Text = "Future Value:";
             // 
             // txtMonthlyInvestment
             // 
-            txtMonthlyInvestment.Location = new Point(194, 40);
+            txtMonthlyInvestment.Location = new Point(170, 30);
+            txtMonthlyInvestment.Margin = new Padding(3, 2, 3, 2);
             txtMonthlyInvestment.Name = "txtMonthlyInvestment";
-            txtMonthlyInvestment.Size = new Size(125, 27);
+            txtMonthlyInvestment.Size = new Size(110, 23);
             txtMonthlyInvestment.TabIndex = 1;
+            txtMonthlyInvestment.TextChanged += txtMonthlyInvestment_TextChanged;
             // 
             // txtInterestRate
             // 
-            txtInterestRate.Location = new Point(194, 79);
+            txtInterestRate.Location = new Point(170, 59);
+            txtInterestRate.Margin = new Padding(3, 2, 3, 2);
             txtInterestRate.Name = "txtInterestRate";
-            txtInterestRate.Size = new Size(125, 27);
+            txtInterestRate.Size = new Size(110, 23);
             txtInterestRate.TabIndex = 2;
             // 
             // txtYears
             // 
-            txtYears.Location = new Point(194, 130);
+            txtYears.Location = new Point(170, 98);
+            txtYears.Margin = new Padding(3, 2, 3, 2);
             txtYears.Name = "txtYears";
-            txtYears.Size = new Size(125, 27);
+            txtYears.Size = new Size(110, 23);
             txtYears.TabIndex = 3;
             // 
             // txtFutureValue
             // 
-            txtFutureValue.Location = new Point(194, 172);
+            txtFutureValue.Location = new Point(170, 129);
+            txtFutureValue.Margin = new Padding(3, 2, 3, 2);
             txtFutureValue.Name = "txtFutureValue";
             txtFutureValue.ReadOnly = true;
-            txtFutureValue.Size = new Size(125, 27);
+            txtFutureValue.Size = new Size(110, 23);
             txtFutureValue.TabIndex = 7;
             txtFutureValue.TabStop = false;
             // 
             // btnCalculate
             // 
-            btnCalculate.Location = new Point(65, 273);
+            btnCalculate.Location = new Point(57, 205);
+            btnCalculate.Margin = new Padding(3, 2, 3, 2);
             btnCalculate.Name = "btnCalculate";
-            btnCalculate.Size = new Size(94, 29);
+            btnCalculate.Size = new Size(82, 22);
             btnCalculate.TabIndex = 4;
             btnCalculate.Text = "Calculate";
             btnCalculate.UseVisualStyleBackColor = true;
@@ -118,9 +124,10 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(182, 273);
+            btnExit.Location = new Point(159, 205);
+            btnExit.Margin = new Padding(3, 2, 3, 2);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(94, 29);
+            btnExit.Size = new Size(82, 22);
             btnExit.TabIndex = 5;
             btnExit.Text = "Exit";
             btnExit.UseVisualStyleBackColor = true;
@@ -128,9 +135,9 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(btnExit);
             Controls.Add(btnCalculate);
             Controls.Add(txtFutureValue);
@@ -141,6 +148,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
